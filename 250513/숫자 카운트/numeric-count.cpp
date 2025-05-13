6fac077487231402eff83cbce6b91d8334bd0c49
jLica <gkswjddnjs15@gmail.com>
2025-05-13 22:08:25 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250513/숫자 카운트/numeric-count.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> v;
+vector<pair<int,int>> v2;
+int N;
+
+pair<int, int> count(int a, int b) {
+    vector<int> v_a, v_b;
+    int tens = 1;
+    for(int i = 0; i < 3; i++) {
+        v_a.push_back((a%(tens*10)-a%tens)/(tens));
+        v_b.push_back((b%(tens*10)-b%tens)/(tens));
+        tens *= 10;
+    }
+
+    int count1 = 0;
+    int count2 = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(v_a[i] == v_b[j]) {
+                if(i == j) {
+                    count1++;
+                }
+                else {
+                    count2++;
+                }
+            }
+        }
+    }
+    return {count1, count2};
+}
+
+bool satisfied(int a, int b, const pair<int, int>& p) {
+    const pair<int, int> p2 = count(a, b);
+
+    if(p.first == p2.first && p.second == p2.second) return true;
+    return false;
+}
+
+int main() {
+    // Please write your code here.
+    cin >> N;
+    int a1, a2, a3;
+    for(int i = 0; i < N; i++) {
+        cin >> a1 >> a2 >> a3;
+        v.push_back(a1);
+        v2.push_back({a2,a3});
+    }
+    int num;
+    int count = 0;
+    bool isSatisfied = true;
+    for(int i = 1; i <= 9; i++) {
+        for(int j = 1; j <= 9; j++) {
+            for(int k = 1; k <= 9; k++) {
+                num = i*100 + j*10 + k;
+                isSatisfied = true;
+                for(int l = 0; l < N; l++) {
+                    if(!satisfied(num, v[l], v2[l])) {
+                        isSatisfied = false;
+                    }
+                }
+                if(isSatisfied) {
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
