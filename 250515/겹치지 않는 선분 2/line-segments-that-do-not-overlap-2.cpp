e9e3a9f76b71df720a6b20b19ad4aa13b5ca3327
jLica <gkswjddnjs15@gmail.com>
2025-05-15 23:46:01 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250515/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int x1[100], x2[100];
+bool overlapped[100];
+
+bool isOverlapped(int i, int j) {
+    return ((x1[j]-x1[i])*(x2[j]-x2[i]) < 0);
+}
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    // Please write your code here.
+    // a1 <= b1 && a2 >= b2
+    // a1 >= b1 && a2 <= b2
+    // (b1-a1)*(b2-a2) <= 0
+    fill(overlapped, overlapped+N, false);
+    int count = 0;
+    for(int i = 0; i < N-1; i++) {
+        for(int j = i+1; j < N; j++) {
+            if(isOverlapped(i, j)) {
+                overlapped[i] = true;
+                overlapped[j] = true;
+            }
+        }
+    }
+    for(int i = 0; i < N; i++) {
+        if(!overlapped[i]) count++;
+    }
+    cout << count;
+    return 0;
+}
