c8557dff3cad9f333a4441fb3f335567c79035d2
jLica <gkswjddnjs15@gmail.com>
2025-05-16 21:22:13 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250516/상해버린 치즈/rotten-cheese.cpp
+++ 250516/상해버린 치즈/rotten-cheese.cpp
@@ -4,15 +4,25 @@
 
 using namespace std;
 
+set<int> intersection(set<int>& s1, set<int>& s2) {
+    set<int> result;
+    for(int item1 : s1) {
+        for(int item2 : s2) {
+            if(item1 == item2) result.insert(item1);
+        }
+    }
+    return result;
+}
+
 int N, M, D, S;
 int p[1000], m[1000], t[1000];
 int sick_p[50], sick_t[50];
-bool bad_cheese[50]; // 치즈마다 얼마나 많은 사람이 먹었는지
 int main() {
     cin >> N >> M >> D >> S;
-
+    set<int> bad_cheese_candidates;
     for (int i = 0; i < D; i++) {
         cin >> p[i] >> m[i] >> t[i];
+        bad_cheese_candidates.insert(m[i]);
     }
 
     for (int i = 0; i < S; i++) {
@@ -21,29 +31,24 @@ int main() {
 
     // Please write your code here.
     //치즈의 수와 사람의 수가 적기 때문에 치즈별로 완전탐색을 하는 것이 좋아 보임
-    fill(bad_cheese, bad_cheese+M, false);
-
-    // 상한 치즈를 구분하는 단계
+    // 아픈 사람이 먹은 공통 치즈
+    set<int> eaten_cheese;
     for(int i = 0; i < S; i++) {
         for(int j = 0; j < D; j++) {
-            if(sick_p[i] == p[j] && sick_t[i] > t[j]) { // 아픈 사람이 아프기 전에 먹은 치즈
-                bad_cheese[m[j]-1] = true;
-            }
+            if(sick_p[i] != p[j] || sick_t[i] <= t[j]) continue;
+            eaten_cheese.insert(m[j]);
         }
+        bad_cheese_candidates = intersection(bad_cheese_candidates, eaten_cheese);
+        eaten_cheese.clear();
     }
-    
     int maxCount = 0;
-    set<int> s;
+    set<int> consumer;
 
-    for(int i = 0; i < M; i++) {
-        if(!bad_cheese[i]) continue;
+    for(int i : bad_cheese_candidates) {
         for(int j = 0; j < D; j++) {
-            if(m[j]-1 == i) { // 상한 것 같은 치즈를 먹은 적이 있다
-                s.insert(p[j]);
-            }
+            if(m[j] == i) consumer.insert(p[j]);
         }
-        maxCount = max(maxCount, int(s.size())); // 가장 많은 사람이 먹은 "상한 것 같은 치즈"
-        s.clear();
+        maxCount = max(maxCount, int(consumer.size()));
     }
     cout << maxCount;
     return 0;
