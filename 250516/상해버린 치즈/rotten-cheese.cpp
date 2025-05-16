0caa24d5161aab475407f4c7d6e5dfa0e705847e
jLica <gkswjddnjs15@gmail.com>
2025-05-16 20:59:37 +0900

    Title: 상해버린 치즈, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250516/상해버린 치즈/rotten-cheese.cpp
+++ 250516/상해버린 치즈/rotten-cheese.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 int N, M, D, S;
 int p[1000], m[1000], t[1000];
 int sick_p[50], sick_t[50];
-bool badCheese[50];
-bool consumedBadCheese[50];
+bool bad_cheese[50]; // 치즈마다 얼마나 많은 사람이 먹었는지
 int main() {
     cin >> N >> M >> D >> S;
 
@@ -21,32 +21,30 @@ int main() {
 
     // Please write your code here.
     //치즈의 수와 사람의 수가 적기 때문에 치즈별로 완전탐색을 하는 것이 좋아 보임
-    fill(badCheese, badCheese+M, false);
-    fill(consumedBadCheese, consumedBadCheese+N, false);
+    fill(bad_cheese, bad_cheese+M, false);
 
     // 상한 치즈를 구분하는 단계
     for(int i = 0; i < S; i++) {
         for(int j = 0; j < D; j++) {
-            if(sick_p[i] == p[j] && sick_t[i] > t[j]) {
-                badCheese[m[j]-1] = true;
+            if(sick_p[i] == p[j] && sick_t[i] > t[j]) { // 아픈 사람이 아프기 전에 먹은 치즈
+                bad_cheese[m[j]-1] = true;
             }
         }
     }
+    
+    int maxCount = 0;
+    set<int> s;
 
-    // 상한 치즈를 먹은 사람을 구분하는 단계
     for(int i = 0; i < M; i++) {
-        if(!badCheese[i]) continue;
-
+        if(!bad_cheese[i]) continue;
         for(int j = 0; j < D; j++) {
-            if(m[j]-1 == i) { // 먹은 치즈가 상한 치즈면 
-                consumedBadCheese[p[j]-1] = true;
+            if(m[j]-1 == i) { // 상한 것 같은 치즈를 먹은 적이 있다
+                s.insert(p[j]);
             }
         }
+        maxCount = max(maxCount, int(s.size())); // 가장 많은 사람이 먹은 "상한 것 같은 치즈"
+        s.clear();
     }
-    int count = 0;
-    for(int i = 0; i < N; i++) {
-        count += consumedBadCheese[i];
-    }
-    cout << count;
+    cout << maxCount;
     return 0;
 }
