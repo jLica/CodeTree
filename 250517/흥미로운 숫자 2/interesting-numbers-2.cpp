c793d7a8a1cbaaa1519b91fc0fe162d75b5a5dc1
jLica <gkswjddnjs15@gmail.com>
2025-05-17 22:59:19 +0900

    Title: 흥미로운 숫자 2, Time: 12ms, Memory: 0MB, Status: Passed - Codetree

--- 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
-#include <unordered_map>
 
 using namespace std;
 
 int X, Y;
-int startIdx;
-
-bool isInteresting(const int num) {
-    unordered_map<int, int> m;
-    int tmp = num;
-    while(tmp > 0) {
-        m[tmp%10]++;
-        tmp /= 10;
-    }
-    if(m.size() != 2) return false;
-    
-    for(const auto& p : m) {
-        if(p.second == 1) return true;
-    }
-    return false;
-}
 
 int main() {
     cin >> X >> Y;
 
     // Please write your code here.
     int count = 0;
-
+    int num, size;
     for(int i = X; i <= Y; i++) {
-        if(isInteresting(i)) count++;
+        int figures[10] = {};
+        size = 0;
+        num = i;
+        while(num > 0) {
+            figures[num%10]++;
+            num /= 10;
+            size++;
+        }
+        for(int j = 0; j < 10; j++) {
+            if(figures[j] == size-1) {
+                count++;
+                break;
+            }
+        }
     }
     cout << count;
     return 0;
