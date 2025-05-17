83f96e3160491fcbd56b198db54061c6f33ca11c
jLica <gkswjddnjs15@gmail.com>
2025-05-17 21:49:08 +0900

    Title: 데이터센터의 온도 조정 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250517/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int N, C, G, H;
+int Ta[1000], Tb[1000];
+
+int main() {
+    cin >> N >> C >> G >> H;
+
+    for (int i = 0; i < N; i++) {
+        cin >> Ta[i] >> Tb[i];
+    }
+
+    // Please write your code here.
+    int sum = 0;
+    int maxSum = 0;
+
+    for(int i = 0; i <= 1000; i++) {
+        sum = 0;
+        for(int j = 0; j < N; j++) {
+            if(i < Ta[j]) sum += C;
+            else if(i >= Ta[j] && i < Tb[j]) sum += G;
+            else sum += H;
+        }
+        maxSum = max(sum, maxSum);
+    }
+    cout << maxSum;
+    return 0;
+}
