1d6361206e3606f68e1ef5a149d3c697ca6f1451
jLica <gkswjddnjs15@gmail.com>
2025-05-17 22:15:18 +0900

    Title: 흥미로운 숫자 2, Time: 77ms, Memory: 0MB, Status: Passed - Codetree

--- 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -5,14 +5,24 @@ using namespace std;
 
 int X, Y;
 
-bool isInteresting(int num) {
-    int tens = 1;
-    int figure;
+void addOne(int* figures) {
+    figures[6]++;
+    for(int i = 6; i >= 0; i--) {
+        if(figures[i] == 10) {
+            figures[i] = 0;
+            figures[i-1]++;
+        }
+    }
+}
+
+bool isInteresting(const int* figures) {
     map<int, int> m;
-    while(num >= tens) {
-        figure = (num/tens)%10;
-        m[figure]++;
-        tens *= 10;
+    int startIdx;
+    for(startIdx = 0; startIdx < 7; startIdx++) {
+        if(figures[startIdx] != 0) break;
+    }
+    for(int i = startIdx; i < 7; i++) {
+        m[figures[i]]++;
     }
     bool result = false;
     if(m.size() == 2) {
@@ -28,8 +38,17 @@ int main() {
 
     // Please write your code here.
     int count = 0;
+    int figures[7];
+
+    int tens = 1;
+    for(int i = 6; i >= 0; i--) {
+        figures[i] = (X/tens)%10;
+        tens *= 10;
+    }
+
     for(int i = X; i <= Y; i++) {
-        if(isInteresting(i)) count++;
+        if(isInteresting(figures)) count++;
+        addOne(figures);
     }
     cout << count;
     return 0;
