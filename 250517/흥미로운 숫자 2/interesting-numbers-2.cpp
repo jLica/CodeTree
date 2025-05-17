3b9f1994cab3914ab93730127672416d0edabe60
jLica <gkswjddnjs15@gmail.com>
2025-05-17 22:45:36 +0900

    Title: 흥미로운 숫자 2, Time: 77ms, Memory: 0MB, Status: Passed - Codetree

--- 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -4,23 +4,29 @@
 using namespace std;
 
 int X, Y;
+int startIdx;
 
-void addOne(int* figures) {
-    figures[6]++;
+void update(int* figures) {
     for(int i = 6; i >= 0; i--) {
         if(figures[i] == 10) {
             figures[i] = 0;
             figures[i-1]++;
+            if(i == startIdx) startIdx--;
         }
     }
 }
 
+void addOne(int* figures) {
+    figures[6]++;
+    if(figures[6] == 10) update(figures);
+}
+
 bool isInteresting(const int* figures) {
     map<int, int> m;
-    int startIdx;
-    for(startIdx = 0; startIdx < 7; startIdx++) {
-        if(figures[startIdx] != 0) break;
-    }
+    //int startIdx;
+    //for(startIdx = 0; startIdx < 7; startIdx++) {
+    //    if(figures[startIdx] != 0) break;
+    //}
     for(int i = startIdx; i < 7; i++) {
         m[figures[i]]++;
     }
@@ -43,6 +49,7 @@ int main() {
     int tens = 1;
     for(int i = 6; i >= 0; i--) {
         figures[i] = (X/tens)%10;
+        if(figures[i] != 0) startIdx = i; // 초기 자릿수
         tens *= 10;
     }
 
