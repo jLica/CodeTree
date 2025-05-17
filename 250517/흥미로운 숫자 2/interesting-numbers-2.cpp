425941e3c4a53814b8a3a5fa60c7a652c1bc13fa
jLica <gkswjddnjs15@gmail.com>
2025-05-17 22:51:19 +0900

    Title: 흥미로운 숫자 2, Time: 85ms, Memory: 0MB, Status: Passed - Codetree

--- 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250517/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 int X, Y;
 int startIdx;
 
-void update(int* figures) {
-    for(int i = 6; i >= 0; i--) {
-        if(figures[i] == 10) {
-            figures[i] = 0;
-            figures[i-1]++;
-            if(i == startIdx) startIdx--;
-        }
+bool isInteresting(const int num) {
+    unordered_map<int, int> m;
+    int tmp = num;
+    while(tmp > 0) {
+        m[tmp%10]++;
+        tmp /= 10;
     }
-}
-
-void addOne(int* figures) {
-    figures[6]++;
-    if(figures[6] == 10) update(figures);
-}
-
-bool isInteresting(const int* figures) {
-    map<int, int> m;
-    //int startIdx;
-    //for(startIdx = 0; startIdx < 7; startIdx++) {
-    //    if(figures[startIdx] != 0) break;
-    //}
-    for(int i = startIdx; i < 7; i++) {
-        m[figures[i]]++;
+    if(m.size() != 2) return false;
+    
+    for(const auto& p : m) {
+        if(p.second == 1) return true;
     }
-    bool result = false;
-    if(m.size() == 2) {
-        for(const auto& p : m) {
-            if(p.second == 1) result = true;
-        }
-    }
-    return result;
+    return false;
 }
 
 int main() {
@@ -44,18 +26,9 @@ int main() {
 
     // Please write your code here.
     int count = 0;
-    int figures[7];
-
-    int tens = 1;
-    for(int i = 6; i >= 0; i--) {
-        figures[i] = (X/tens)%10;
-        if(figures[i] != 0) startIdx = i; // 초기 자릿수
-        tens *= 10;
-    }
 
     for(int i = X; i <= Y; i++) {
-        if(isInteresting(figures)) count++;
-        addOne(figures);
+        if(isInteresting(i)) count++;
     }
     cout << count;
     return 0;
