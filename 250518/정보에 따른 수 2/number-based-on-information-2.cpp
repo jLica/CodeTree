fae9bd3e4e48a91ca6a704899aba896b27acc0ee
jLica <gkswjddnjs15@gmail.com>
2025-05-18 22:37:16 +0900

    Title: 정보에 따른 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250518/정보에 따른 수 2/number-based-on-information-2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int T, a, b;
+char c[1000];
+
+int nearestDistance(int position, char alphabet) {
+    int min_distance = INT_MAX;
+    int distance;
+
+    for(int i = position; i >= 0; i--) {
+        if(c[i] == alphabet) {
+            distance = position-i;
+            min_distance = distance;
+            break;
+        }
+    }
+    for(int i = position; i < 1000; i++) {
+        if(c[i] == alphabet) {
+            distance = i-position;
+            min_distance = min(distance, min_distance);
+            break;
+        }
+    }
+    return min_distance;
+}
+
+bool isSatisfied(int position) {
+    int distance_S = nearestDistance(position, 'S');
+    int distance_N = nearestDistance(position, 'N');
+    return (distance_S <= distance_N);
+}
+
+int main() {
+    cin >> T >> a >> b;
+
+    int position;
+    char alphabet;
+
+    fill(c, c+1000, 'X');
+
+    for (int i = 0; i < T; i++) {
+        cin >> alphabet >> position;
+        c[position-1] = alphabet;
+    }
+
+    // Please write your code here.
+    int count = 0;
+    for(int i = a; i <= b; i++) {
+        if(isSatisfied(i)) count++;
+    }
+    cout << count;
+    return 0;
+}
