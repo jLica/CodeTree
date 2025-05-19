ca11648538ef71d05444040a6ed3439859628ab7
jLica <gkswjddnjs15@gmail.com>
2025-05-19 15:24:13 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250519/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B, C;
+
+int main() {
+    cin >> A >> B >> C;
+
+    // Please write your code here.
+    int max_value = 0;
+    int value = 0;
+    for(int i = 0; i <= C/A+1; i++) {
+        value = A*i;
+        for(int j = 0; j <= C/B+1; j++) {
+            value += B*j;
+            if(value <= C)
+                max_value = max(value, max_value);
+        }
+    }
+    cout << max_value;
+    return 0;
+}
