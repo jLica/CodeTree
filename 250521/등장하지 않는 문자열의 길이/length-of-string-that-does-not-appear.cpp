9ababfb45bc5cf42d6bf3351dbd5d36754d7b534
jLica <gkswjddnjs15@gmail.com>
2025-05-21 19:09:14 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250521/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 250521/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -14,18 +14,18 @@ int main() {
     // Please write your code here.
     // 문자열 내의 어떤 substring도 반복되지 않는 최소 길이
     for(int i = 1; i <= N; i++) {
-        bool isOnlyOne = true;
+        bool is_only_one = true;
         unordered_set<string> s;
         
         for(int j = 0; j <= N-i; j++) {
             string sub_str = str.substr(j, i); // i 길이의 substr
             if(s.count(sub_str) == 1) {
-                isOnlyOne = false;
+                is_only_one = false;
                 break;
             }
             s.insert(sub_str);
         }
-        if(isOnlyOne) {
+        if(is_only_one) {
             cout << i;
             return 0;
         }
