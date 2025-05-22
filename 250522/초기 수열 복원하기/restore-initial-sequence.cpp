b14bdbb94b5f9bafd81f456e7b4d0c277436b6f9
jLica <gkswjddnjs15@gmail.com>
2025-05-22 23:35:39 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250522/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int adjacent[1000];
+int original[1000];
+
+bool isOverlapped(int num, int idx) {
+    for(int i = 0; i <= idx; i++) {
+        if(original[i] == num) return true;
+    }
+    return false;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> adjacent[i];
+    }
+
+    // Please write your code here.
+    for(int i = 1; i < adjacent[0]; i++) {
+        // 앞에서부터 가능한 조합을 만들어가며 진행
+        original[0] = i;
+        for(int j = 0; j < n-1; j++) {
+            int next = adjacent[j] - original[j];
+            if(isOverlapped(next, j)) break;
+            original[j+1] = next;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        cout << original[i] << " ";
+    }
+    return 0;
+}
